442d80333db1e9fdf53d8abe7913d090d7e8ca5c
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-06 20:55:42 -0300

    feat: rename CleaningServiceDaoTest

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/tests/com/uefs/sistemadegerenciamento/dao/cleaning_service/CleaningServiceDaoTest.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/tests/com/uefs/sistemadegerenciamento/dao/cleaning_service/CleaningServiceDaoTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CleaningServiceTest {
+class CleaningServiceDaoTest {
     private CleaningServiceDao cleaningServiceDao;
     private CleaningService cleaningService;
 
