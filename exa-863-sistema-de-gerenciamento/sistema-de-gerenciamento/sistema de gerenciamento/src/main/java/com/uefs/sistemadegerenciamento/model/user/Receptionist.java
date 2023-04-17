64981a17dbc658d973a8e2ea0f026ea04410ea3f
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 12:16:59 -0300

    docs(Receptionist): improved JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/user/Receptionist.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/user/Receptionist.java
@@ -3,7 +3,15 @@ package com.uefs.sistemadegerenciamento.model.user;
 import com.uefs.sistemadegerenciamento.constants.UserType;
 
 /**
- * Classe que representa um Recepionista
+ * <p>
+ *     Classe que representa um administrador do sistema.
+ * </p>
+ * <p>
+ *     A classe herda de {@link User} e possui um construtor que recebe o nome, email e senha do usuário.
+ *     O tipo do usuário é definido como {@link UserType#RECEPTIONIST}.
+ * </p>
+ *
+ * @see User
  */
 public class Receptionist extends User {
     public Receptionist(
