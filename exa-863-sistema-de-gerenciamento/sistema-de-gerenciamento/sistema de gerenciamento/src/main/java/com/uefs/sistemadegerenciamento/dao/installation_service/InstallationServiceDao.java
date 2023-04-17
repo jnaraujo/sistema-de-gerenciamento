4d115b74d4d0cce520f7b57b7e8b17d3e22db074
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:44:31 -0300

    docs(InstallationServiceDao): add JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/installation_service/InstallationServiceDao.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/installation_service/InstallationServiceDao.java
@@ -3,5 +3,8 @@ package com.uefs.sistemadegerenciamento.dao.installation_service;
 import com.uefs.sistemadegerenciamento.dao.Dao;
 import com.uefs.sistemadegerenciamento.model.service.InstallationService;
 
+/**
+ * Interface que define os métodos de acesso a dados da classe {@link InstallationService}
+ */
 public interface InstallationServiceDao extends Dao<InstallationService> {
 }
