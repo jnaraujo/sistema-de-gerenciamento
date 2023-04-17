b11364cda585873735227d1c2d4e5851508ac3e1
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 12:24:08 -0300

    docs(IdGenerator): improved JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/utils/IdGenerator.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/utils/IdGenerator.java
@@ -1,7 +1,20 @@
 package com.uefs.sistemadegerenciamento.utils;
 
+/**
+ * <p>
+ *     Classe que gera um id aleatório
+ * </p>
+ * <p>
+ *     A classe gera um id aleatório utilizando a classe {@link java.util.UUID}
+ *     e retorna uma string com o id gerado.
+ * </p>
+ *
+ * @see java.util.UUID
+ * @author Jônatas Araújo
+ */
 public class IdGenerator {
     /**
+     * Gera um id aleatório
      * @return Retorna um id gerado aleatoriamente
      */
     public static String generate() {
