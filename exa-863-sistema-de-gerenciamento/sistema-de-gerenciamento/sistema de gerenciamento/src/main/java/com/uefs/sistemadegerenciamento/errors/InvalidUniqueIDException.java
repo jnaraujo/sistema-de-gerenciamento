7298f3f5f17f399aba68db97c1b9f94c8ce087a4
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:59:14 -0300

    feat: removed InvalidUniqueIDException.java

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/errors/InvalidUniqueIDException.java
+++
@@ -1,7 +0,0 @@
-package com.uefs.sistemadegerenciamento.errors;
-
-public class InvalidUniqueIDException extends Exception{
-    public InvalidUniqueIDException(String message){
-        super(message);
-    }
-}
