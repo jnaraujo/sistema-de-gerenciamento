a242d8803ca0c78948d495c5db177c2ff6f9ebf7
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:40:52 -0300

    docs(UserType): add JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/constants/UserType.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/constants/UserType.java
@@ -1,7 +1,21 @@
 package com.uefs.sistemadegerenciamento.constants;
 
+/**
+ * Enumeração que representa os tipos de usuários do sistema.
+ */
 public enum UserType {
+    /**
+     * Tipo do usuário administrador.
+     */
     ADMINISTRATOR,
+
+    /**
+     * Tipo do usuário técnico.
+     */
     TECHNICIAN,
+
+    /**
+     * Tipo do usuário recepcionista.
+     */
     RECEPTIONIST
 }
