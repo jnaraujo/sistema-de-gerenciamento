c5ac9e83a335d4227c2ed508aeeff26d5d60395e
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:50:43 -0300

    docs(WorkOrderDao): improved JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/workorder/WorkOrderDao.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/workorder/WorkOrderDao.java
@@ -5,42 +5,51 @@ import com.uefs.sistemadegerenciamento.model.WorkOrder;
 
 import java.util.List;
 
+/**
+ * Interface que define os métodos de acesso a dados de ordens de serviço.
+ * @see WorkOrder
+ */
 public interface WorkOrderDao extends Dao<WorkOrder> {
     /**
      * Retorna uma lista de ordens de serviço abertas por ordem de criação (mais antiga primeiro)
-     * @return Lista de ordens de serviço abertas
+     * @return lista de ordens de serviço abertas
      */
     List<WorkOrder> findOpenWorkOrders();
 
     /**
      * @param technicianId ID do técnico
-     * @return Retorna a ordem de serviço do técnico
+     * @return ordem de serviço do técnico
      */
     WorkOrder findOrderByTechnicianId(String technicianId);
 
     /**
      * Retorna o tempo médio de espera para reparo de uma ordem de serviço
+     * @return Tempo médio de espera para reparo de uma ordem de serviço
      */
     Double getAverageTimeInHoursToRepair();
 
     /**
      * Retorna o tempo médio de espera para reparo de uma ordem de serviço por técnico
      * @param technicianId ID do técnico
+     * @return tempo médio de espera para reparo de uma ordem de serviço por técnico
      */
     Double getAverageTimeInHoursToRepairByTechnician(String technicianId);
 
     /**
      * Retorna o custo médio de uma ordem de serviço
+     * @return custo médio de uma ordem de serviço
      */
     Double getAverageWorkOrderCost();
 
     /**
      * Retorna o preço médio de uma ordem de serviço
+     * @return preço médio de uma ordem de serviço
      */
     Double getAverageWorkOrderPrice();
 
     /**
      * Retorna a satisfação média dos clientes
+     * @return satisfação média dos clientes
      */
     Double getAverageCustomerSatifaction();
 }
