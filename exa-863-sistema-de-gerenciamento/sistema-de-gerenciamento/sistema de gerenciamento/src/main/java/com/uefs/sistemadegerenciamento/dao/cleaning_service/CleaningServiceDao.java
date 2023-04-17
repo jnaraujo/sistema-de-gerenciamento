bcb2279ab40a62778f3d45760500da8959ab5b86
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:42:37 -0300

    docs(CleaningServiceDao): add JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/cleaning_service/CleaningServiceDao.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/cleaning_service/CleaningServiceDao.java
@@ -3,5 +3,9 @@ package com.uefs.sistemadegerenciamento.dao.cleaning_service;
 import com.uefs.sistemadegerenciamento.dao.Dao;
 import com.uefs.sistemadegerenciamento.model.service.CleaningService;
 
+/**
+ * A interface CleaningServiceDao é responsável por definir as operações
+ * de persistência para a entidade CleaningService.
+ */
 public interface CleaningServiceDao extends Dao<CleaningService> {
 }
