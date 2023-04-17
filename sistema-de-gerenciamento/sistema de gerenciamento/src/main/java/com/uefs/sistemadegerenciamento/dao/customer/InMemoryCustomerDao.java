c83ac4f5b75b55b9341b168d4a6989e154f17335
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 18:28:57 -0300

    docs(InMemoryCustomerDao): improved docs

--- sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/customer/InMemoryCustomerDao.java
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/customer/InMemoryCustomerDao.java
@@ -16,7 +16,20 @@ import java.util.List;
  * @author Jônatas Araújo
  */
 public class InMemoryCustomerDao implements CustomerDao {
-    private HashMap<String, Customer> customers = new HashMap<>();
+    private HashMap<String, Customer> customers;
+
+    /**
+     * Cria um novo {@link InMemoryCustomerDao}.
+     */
+    public InMemoryCustomerDao(){
+        customers = new HashMap<>();
+    }
+
+    /**
+     * Salva um cliente.
+     * @param customer Cliente a ser salvo.
+     * @return Cliente salvo com o ID gerado.
+     */
     @Override
     public Customer save(Customer customer) {
         String id = IdGenerator.generate();
@@ -27,26 +40,46 @@ public class InMemoryCustomerDao implements CustomerDao {
         return customer;
     }
 
+    /**
+     * Deleta um cliente pelo ID.
+     * @param customerID ID do cliente a ser deletado.
+     */
     @Override
     public void delete(String customerID) {
         customers.remove(customerID);
     }
 
+    /**
+     * Atualiza um cliente.
+     * @param customer Cliente a ser atualizado.
+     */
     @Override
     public void update(Customer customer) {
         customers.replace(customer.getId(), customer);
     }
 
+    /**
+     * Busca um cliente pelo ID.
+     * @param customerID ID do cliente a ser buscado.
+     * @return Cliente encontrado.
+     */
     @Override
     public Customer findById(String customerID) {
         return customers.get(customerID);
     }
 
+    /**
+     * Busca todos os clientes.
+     * @return Todos os clientes.
+     */
     @Override
     public List<Customer> getAll() {
         return new ArrayList<>(customers.values());
     }
 
+    /**
+     * Deleta todos os clientes.
+     */
     @Override
     public void deleteAll() {
         customers.clear();
