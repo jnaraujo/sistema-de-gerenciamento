7eb5f03d4fcfe1026c50e5171412f28c4887d7f1
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 12:15:39 -0300

    docs(Administrator): improved JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/user/Administrator.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/user/Administrator.java
@@ -3,9 +3,25 @@ package com.uefs.sistemadegerenciamento.model.user;
 import com.uefs.sistemadegerenciamento.constants.UserType;
 
 /**
- * Classe que representa um Administrador
+ * <p>
+ *     Classe que representa um administrador do sistema.
+ * </p>
+ * <p>
+ *     A classe herda de {@link User} e possui um construtor que recebe o nome, email e senha do usuário.
+ * </p>
+ * <p>
+ *     O tipo do usuário é definido como {@link UserType#ADMINISTRATOR}.
+ * </p>
+ *
+ * @see User
  */
 public class Administrator extends User {
+    /**
+     * Criar um administrador
+     * @param name Nome do usuário
+     * @param email Email do usuário
+     * @param password Senha do usuário
+     */
     public Administrator(
         String name,
         String email,
