cff1ff254e358677f91130da819e90b3458d96bb
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 12:07:33 -0300

    docs(Service): improved JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/service/Service.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/service/Service.java
@@ -1,16 +1,30 @@
 package com.uefs.sistemadegerenciamento.model.service;
 
 /**
- * Interface que representa um serviço
+ * <p>
+ * Interface que representa um serviço prestado.
+ * Os serviços podem ser de construção, limpeza ou instalação.
+ * </p>
+ *
+ * <p>
+ * Eles serão usados nas {@link com.uefs.sistemadegerenciamento.model.WorkOrder ordens de serviço}.
+ * </p>
+ *
+ * @see BuildingService
+ * @see CleaningService
+ * @see InstallationService
+ * @see com.uefs.sistemadegerenciamento.model.WorkOrder
  */
 public interface Service {
     /**
-     * @return Retorna o preço do serviço
+     * Retorna o preço do serviço
+     * @return o preço do serviço
      */
     public double getPrice();
 
     /**
-     * @return Retorna o custo do serviço
+     * Retorna o custo do serviço
+     * @return o custo do serviço
      */
     public double getCost() ;
 }
