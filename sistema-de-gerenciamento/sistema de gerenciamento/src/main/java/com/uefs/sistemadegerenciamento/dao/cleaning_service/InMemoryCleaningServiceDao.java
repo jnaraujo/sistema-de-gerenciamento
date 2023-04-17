e57a59c687e191074fa6e02998a5ef8b166e37fd
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 18:26:55 -0300

    docs(InMemoryCleaningServiceDao): improved docs

--- sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/cleaning_service/InMemoryCleaningServiceDao.java
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/cleaning_service/InMemoryCleaningServiceDao.java
@@ -19,10 +19,18 @@ public class InMemoryCleaningServiceDao implements CleaningServiceDao{
 
     HashMap<String, CleaningService> cleaningServices;
 
+    /**
+     * Cria um novo objeto {@link InMemoryCleaningServiceDao}
+     */
     public InMemoryCleaningServiceDao() {
         cleaningServices = new HashMap<>();
     }
 
+    /**
+     * Salva um serviço de limpeza no banco de dados
+     * @param cleaningService o serviço de limpeza a ser salvo
+     * @return o serviço de limpeza salvo
+     */
     @Override
     public CleaningService save(CleaningService cleaningService) {
         String id = IdGenerator.generate();
@@ -33,26 +41,46 @@ public class InMemoryCleaningServiceDao implements CleaningServiceDao{
         return cleaningService;
     }
 
+    /**
+     * Deleta um serviço de limpeza do banco de dados
+     * @param id o id do serviço de limpeza a ser deletado
+     */
     @Override
     public void delete(String id) {
         cleaningServices.remove(id);
     }
 
+    /**
+     * Atualiza um serviço de limpeza no banco de dados
+     * @param cleaningService o serviço de limpeza a ser atualizado
+     */
     @Override
     public void update(CleaningService cleaningService) {
         cleaningServices.put(cleaningService.getId(), cleaningService);
     }
 
+    /**
+     * Busca um serviço de limpeza no banco de dados
+     * @param id o id do serviço de limpeza a ser buscado
+     * @return o serviço de limpeza com o id passado como parâmetro
+     */
     @Override
     public CleaningService findById(String id) {
         return cleaningServices.get(id);
     }
 
+    /**
+     * Retorna todos os serviços de limpeza do banco de dados
+     * @return todos os serviços de limpeza do banco de dados
+     */
     @Override
     public List<CleaningService> getAll() {
         return new ArrayList<>(cleaningServices.values());
     }
 
+    /**
+     * Deleta todos os serviços de limpeza do banco de dados
+     */
     @Override
     public void deleteAll() {
         cleaningServices.clear();
