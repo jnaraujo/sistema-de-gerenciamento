f6235f22e87ec7315ed69f3e84c384626633e3ef
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:58:46 -0300

    docs(InvalidSatisfactionScoreException): add JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/errors/InvalidSatisfactionScoreException.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/errors/InvalidSatisfactionScoreException.java
@@ -1,6 +1,13 @@
 package com.uefs.sistemadegerenciamento.errors;
 
+/**
+ * Exceção lançada quando um valor inválido de satisfação é atribuído a um objeto.
+ */
 public class InvalidSatisfactionScoreException extends Exception{
+    /**
+     * Cria uma nova instância de InvalidSatisfactionScoreException com a mensagem de erro especificada.
+     * @param message mensagem de erro
+     */
     public InvalidSatisfactionScoreException(String message) {
         super(message);
     }
