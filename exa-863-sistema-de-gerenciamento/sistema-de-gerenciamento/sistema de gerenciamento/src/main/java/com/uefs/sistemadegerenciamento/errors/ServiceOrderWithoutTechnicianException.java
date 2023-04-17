e9455099f031b9973e7880b232fb5d2426dd4fe0
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 12:00:02 -0300

    docs(ServiceOrderWithoutTechnicianException): improved JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/errors/ServiceOrderWithoutTechnicianException.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/errors/ServiceOrderWithoutTechnicianException.java
@@ -1,6 +1,13 @@
 package com.uefs.sistemadegerenciamento.errors;
 
+/**
+ * Exceção lançada quando uma ordem de serviço não possui um técnico.
+ */
 public class ServiceOrderWithoutTechnicianException extends Exception{
+    /**
+     * Cria uma nova instância de ServiceOrderWithoutTechnicianException com a mensagem de erro especificada.
+     * @param message mensagem de erro
+     */
     public ServiceOrderWithoutTechnicianException(String message) {
         super(message);
     }
