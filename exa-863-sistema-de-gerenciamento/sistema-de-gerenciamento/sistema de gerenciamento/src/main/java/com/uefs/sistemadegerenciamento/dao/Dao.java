0293782ce2354b993c83453d0e5d59599790e136
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:54:12 -0300

    docs(Dao): improved JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/Dao.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/Dao.java
@@ -2,35 +2,46 @@ package com.uefs.sistemadegerenciamento.dao;
 
 import java.util.List;
 
+/**
+ * Interface para um DAO genérico que contém as operações básicas de persistência de um objeto.
+ * @param <T> o tipo de objeto que será salvo
+ */
 public interface Dao<T> {
     /**
+     * Salva um objeto no banco de dados.
+     * Se o objeto já existir, ele será atualizado.
      * @param t o objeto a ser salvo
+     * @return o objeto salvo
      */
     T save(T t);
 
     /**
+     * Deleta um objeto do banco de dados.
      * @param id o id do objeto a ser deletado
      */
     void delete(String id);
 
     /**
+     * Atualiza um objeto no banco de dados.
      * @param t o objeto a ser atualizado
      */
     void update(T t);
 
     /**
+     * Busca um objeto pelo id.
      * @param id o id do objeto a ser buscado
-     * @return o objeto com o id passado
+     * @return o objeto encontrado, ou null caso não exista
      */
     T findById(String id);
 
     /**
-     * @return todos os objetos
+     * Busca todos os objetos do tipo T no banco de dados.
+     * @return uma lista com todos os objetos do tipo T
      */
     List<T> getAll();
 
     /**
-     * Deleta todos os objetos
+     * Deleta todos os objetos do tipo T do banco de dados.
      */
     void deleteAll();
 }
