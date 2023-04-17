b913fcdf5f64d7b4cc330cb2a0e19d0a3592cf95
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 18:31:49 -0300

    docs(InMemoryInventoryDao): improved docs

--- sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/inventory/InMemoryInventoryDao.java
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/inventory/InMemoryInventoryDao.java
@@ -19,10 +19,18 @@ public class InMemoryInventoryDao implements InventoryDao {
 
     private HashMap<String, ComputerComponent> components;
 
+    /**
+     * Cria um novo {@link InMemoryInventoryDao}.
+     */
     public InMemoryInventoryDao() {
         components = new HashMap<>();
     }
 
+    /**
+     * Salva um componente.
+     * @param component Componente a ser salvo.
+     * @return Componente salvo com o ID gerado.
+     */
     @Override
     public ComputerComponent save(ComputerComponent component) {
         String id = IdGenerator.generate();
@@ -33,31 +41,55 @@ public class InMemoryInventoryDao implements InventoryDao {
         return component;
     }
 
+    /**
+     * Deleta um componente pelo ID.
+     * @param id ID do componente a ser deletado.
+     */
     @Override
     public void delete(String id) {
         components.remove(id);
     }
 
+    /**
+     * Atualiza um componente.
+     * @param component Componente a ser atualizado.
+     */
     @Override
     public void update(ComputerComponent component) {
         components.put(component.getId(), component);
     }
 
+    /**
+     * Busca um componente pelo ID.
+     * @param id ID do componente a ser buscado.
+     * @return Componente com o ID informado.
+     */
     @Override
     public ComputerComponent findById(String id) {
         return components.get(id);
     }
 
+    /**
+     * Busca um componente pelo nome.
+     * @return Todos os componentes.
+     */
     @Override
     public List<ComputerComponent> getAll() {
         return new ArrayList<>(components.values());
     }
 
+    /**
+     * Deleta todos os componentes.
+     */
     @Override
     public void deleteAll() {
         components.clear();
     }
 
+    /**
+     * Retorna a média de preço dos componentes.
+     * @return A média de preço dos componentes.
+     */
     @Override
     public double getAveragePrice() {
         double average = 0;
@@ -71,6 +103,10 @@ public class InMemoryInventoryDao implements InventoryDao {
         return average / quantity;
     }
 
+    /**
+     * Retorna a média de custo dos componentes.
+     * @return A média de custo dos componentes.
+     */
     @Override
     public double getAverageCost() {
         double average = 0;
@@ -83,6 +119,15 @@ public class InMemoryInventoryDao implements InventoryDao {
         return average / quantity;
     }
 
+    /**
+     * <p>
+     *     Retorna todos os componentes disponíveis.
+     * </p>
+     * <p>
+     *     Um componente está disponível se sua quantidade for maior que 0.
+     * </p>
+     * @return Todos os componentes disponíveis.
+     */
     @Override
     public List<ComputerComponent> findAvailableComponents() {
         List<ComputerComponent> availableComponents = new ArrayList<>();
