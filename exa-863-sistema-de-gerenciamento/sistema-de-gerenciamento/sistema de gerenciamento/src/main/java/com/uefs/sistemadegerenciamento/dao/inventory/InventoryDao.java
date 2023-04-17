43ea714500c7db9cd16399f138004ac34543f7b3
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:45:50 -0300

    docs(InventoryDao): add JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/inventory/InventoryDao.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/inventory/InventoryDao.java
@@ -5,8 +5,26 @@ import com.uefs.sistemadegerenciamento.model.component.ComputerComponent;
 
 import java.util.List;
 
+/**
+ * Interface que define os métodos de acesso a dados da classe {@link ComputerComponent}.
+ * InventoryDao representa o estoque de componentes da empresa.
+ */
 public interface InventoryDao extends Dao<ComputerComponent> {
+    /**
+     * Retorna a média de preço dos componentes
+     * @return média de preço dos componentes
+     */
     double getAveragePrice();
+
+    /**
+     * Retorna a média de custo dos componentes
+     * @return média de custo dos componentes
+     */
     double getAverageCost();
+
+    /**
+     * Retorna a média de preço dos componentes disponíveis
+     * @return lista de componentes disponíveis
+     */
     List<ComputerComponent> findAvailableComponents();
 }
