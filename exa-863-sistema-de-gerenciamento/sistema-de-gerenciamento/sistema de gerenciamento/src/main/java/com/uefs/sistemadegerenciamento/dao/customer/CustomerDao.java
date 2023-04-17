0680607bf86508220ab13e3f9d36456ecf49d65c
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:43:54 -0300

    docs(CustomerDao): add JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/customer/CustomerDao.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/customer/CustomerDao.java
@@ -3,5 +3,8 @@ package com.uefs.sistemadegerenciamento.dao.customer;
 import com.uefs.sistemadegerenciamento.dao.Dao;
 import com.uefs.sistemadegerenciamento.model.Customer;
 
+/**
+ * Interface que representa o DAO de {@link Customer}.
+ */
 public interface CustomerDao extends Dao<Customer> {
 }
