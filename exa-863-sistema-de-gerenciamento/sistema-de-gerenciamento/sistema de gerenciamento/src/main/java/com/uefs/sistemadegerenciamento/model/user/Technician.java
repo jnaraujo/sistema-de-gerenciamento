39f2e8dc95c75f6b74b5adb6010259d6485227bb
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 12:17:54 -0300

    docs(Technician): improved JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/user/Technician.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/model/user/Technician.java
@@ -3,9 +3,23 @@ package com.uefs.sistemadegerenciamento.model.user;
 import com.uefs.sistemadegerenciamento.constants.UserType;
 
 /**
- * Classe que representa um técnico
+ * <p>
+ *     Classe que representa um técnico no sistema.
+ * </p>
+ * <p>
+ *     A classe herda de {@link User} e possui um construtor que recebe o nome, email e senha do usuário.
+ *     O tipo do usuário é definido como {@link UserType#TECHNICIAN}.
+ * </p>
+ *
+ * @see User
  */
 public class Technician extends User {
+    /**
+     * Criar um técnico com nome, email e senha.
+     * @param name Nome do técnico
+     * @param email Email do técnico
+     * @param password Senha do técnico
+     */
     public Technician(
         String name,
         String email,
