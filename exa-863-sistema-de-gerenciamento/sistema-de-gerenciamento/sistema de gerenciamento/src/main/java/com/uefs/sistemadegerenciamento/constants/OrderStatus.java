10e7b1c1a3c8c684c9d82d0c87702a476cd15fd8
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 11:40:11 -0300

    docs(OrderStatus): add JavaDocs

--- exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/constants/OrderStatus.java
+++ exa-863-sistema-de-gerenciamento/sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/constants/OrderStatus.java
@@ -1,7 +1,21 @@
 package com.uefs.sistemadegerenciamento.constants;
 
+/**
+ * Classe que contém os status possíveis de uma WorkOrder
+ */
 public class OrderStatus {
+    /**
+     * Status de uma WorkOrder que está em andamento
+     */
     public static final String OPEN = "Em andamento";
+
+    /**
+     * Status de uma WorkOrder que foi finalizada
+     */
     public static final String CLOSED = "Finalizado";
+
+    /**
+     * Status de uma WorkOrder que foi cancelada
+     */
     public static final String CANCELED = "Cancelado";
 }
