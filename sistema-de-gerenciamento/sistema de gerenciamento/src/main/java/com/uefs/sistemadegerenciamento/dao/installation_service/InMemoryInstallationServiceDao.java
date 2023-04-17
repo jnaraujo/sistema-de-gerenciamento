56dbd2e7d23a6904bebd4fd97a4e4fe84f0c2e31
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-17 18:30:03 -0300

    docs(InMemoryInstallationServiceDao): improved docs

--- sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/installation_service/InMemoryInstallationServiceDao.java
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/installation_service/InMemoryInstallationServiceDao.java
@@ -19,10 +19,18 @@ public class InMemoryInstallationServiceDao implements InstallationServiceDao {
 
     HashMap<String, InstallationService> services;
 
+    /**
+     * Cria um novo {@link InMemoryInstallationServiceDao}.
+     */
     public InMemoryInstallationServiceDao() {
         services = new HashMap<>();
     }
 
+    /**
+     * Salva um service.
+     * @param installationService Service a ser salvo.
+     * @return Service salvo com o ID gerado.
+     */
     @Override
     public InstallationService save(InstallationService installationService) {
         String id = IdGenerator.generate();
@@ -33,26 +41,46 @@ public class InMemoryInstallationServiceDao implements InstallationServiceDao {
         return installationService;
     }
 
+    /**
+     * Deleta um service pelo ID.
+     * @param id ID do service a ser deletado.
+     */
     @Override
     public void delete(String id) {
         services.remove(id);
     }
 
+    /**
+     * Atualiza um service.
+     * @param installationService Service a ser atualizado.
+     */
     @Override
     public void update(InstallationService installationService) {
         services.put(installationService.getId(), installationService);
     }
 
+    /**
+     * Busca um service pelo ID.
+     * @param id ID do service a ser buscado.
+     * @return Service com o ID informado.
+     */
     @Override
     public InstallationService findById(String id) {
         return services.get(id);
     }
 
+    /**
+     * Busca todos os services cadastrados.
+     * @return Todos os services cadastrados.
+     */
     @Override
     public List<InstallationService> getAll() {
         return new ArrayList<>(services.values());
     }
 
+    /**
+     * Deleta todos os services cadastrados.
+     */
     @Override
     public void deleteAll() {
         services.clear();
