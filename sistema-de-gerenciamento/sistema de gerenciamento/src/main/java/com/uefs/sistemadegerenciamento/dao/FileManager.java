97dd58d42fe65775b0624ff66d4ead62a6bd5aaf
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-04-23 18:56:20 -0300

    feat: add FileManager

---
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/dao/FileManager.java
@@ -0,0 +1,42 @@
+package com.uefs.sistemadegerenciamento.dao;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+
+public class FileManager<K, T> {
+    private String FILE_NAME;
+
+    public FileManager(String fileName) {
+        FILE_NAME = fileName;
+
+        boolean doesFileExist = new java.io.File(FILE_NAME).exists();
+        if (!doesFileExist) {
+            save(new HashMap<K, T>());
+        }
+    }
+
+    public void save(HashMap<K, T> objects) {
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(FILE_NAME));
+            objectOutputStream.writeObject(objects);
+            objectOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<K, T> load() {
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(FILE_NAME));
+            HashMap<K, T> objects = (HashMap<K, T>) objectInputStream.readObject();
+            objectInputStream.close();
+            return objects;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
