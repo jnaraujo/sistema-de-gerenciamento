74035a21f849f8298071e9749721fdb7b1aaf7b1
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-05-09 14:25:25 -0300

    feat: improved Home view

--- sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/controllers/HomeController.java
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/controllers/HomeController.java
@@ -3,15 +3,20 @@ package com.uefs.sistemadegerenciamento.controllers;
 import com.uefs.sistemadegerenciamento.model.user.User;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 public class HomeController {
     User user;
 
     @FXML
-    public Label titleText;
+    public Label titleLabel;
+
+    @FXML
+    public Text userRoleText;
 
     public void setUser(User user) {
         this.user = user;
-        titleText.setText("Bem vindo, " + user.getName()+ "!");
+        titleLabel.setText("Bem vindo, " + user.getName()+ "!");
+        userRoleText.setText("Seu cargo atual: " + user.getUserType().toString());
     }
 }
