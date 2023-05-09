d92a53d59a6db9094548e864edf0f2b4031284ef
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-05-09 13:43:44 -0300

    feat: add Home view

---
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/controllers/HomeController.java
@@ -0,0 +1,8 @@
+package com.uefs.sistemadegerenciamento.controllers;
+
+import com.uefs.sistemadegerenciamento.model.user.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HomeController {
+}
