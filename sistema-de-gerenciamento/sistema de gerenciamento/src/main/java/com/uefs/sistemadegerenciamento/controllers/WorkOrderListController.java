4f7ec974e990fe42a9f1f4596e9ff77efc92d3fb
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-05-10 15:03:29 -0300

    feat(WorkOrderListController): add createEmptyWorkOrderComponent

--- sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/controllers/WorkOrderListController.java
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/controllers/WorkOrderListController.java
@@ -53,15 +53,25 @@ public class WorkOrderListController {
     }
 
     private void fetchWorkOrders() {
+        technicianCurrentWorkOrderVBox.getChildren().clear();
+        workOrderListVBox.getChildren().clear();
+
         List<WorkOrder> workOrders = DAOManager.getWorkOrderDao().findOpenWorkOrders();
         openWorkOrders = workOrders;
 
         boolean doesTechnicianHaveWorkOrder = DAOManager.getWorkOrderDao().findOrderByTechnicianId(user.getId()) != null;
         boolean isButtonDisabled = doesTechnicianHaveWorkOrder;
 
-        workOrderListVBox.getChildren().clear();
         for (WorkOrder workOrder : workOrders) {
             workOrderListVBox.getChildren().add(createWorkOrderComponent(workOrder, isButtonDisabled, false));
+        }
+
+        if(workOrders.isEmpty()) {
+            workOrderListVBox.getChildren().add(createEmptyWorkOrderComponent());
+        }
+
+        if(!doesTechnicianHaveWorkOrder){
+            technicianCurrentWorkOrderVBox.getChildren().add(createEmptyWorkOrderComponent());
         }
 
         if (doesTechnicianHaveWorkOrder) {
@@ -137,6 +147,31 @@ public class WorkOrderListController {
         }
 
         hBox.getChildren().addAll(vBox, button);
+        return hBox;
+    }
+
+    private HBox createEmptyWorkOrderComponent(){
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPrefHeight(80);
+        hBox.setPrefWidth(750);
+        hBox.setPadding(new javafx.geometry.Insets(8, 8, 8, 8));
+        hBox.setStyle("-fx-background-color: #f4f4f4; -fx-border-color: #d4d4d4; -fx-border-width: 1px; -fx-border-radius: 8px;");
+        hBox.setAlignment(Pos.CENTER);
+
+        VBox vBox = new VBox();
+        vBox.prefWidth(600);
+        vBox.setAlignment(Pos.CENTER);
+
+        vBox.setPadding(new javafx.geometry.Insets(0, 8, 0, 0));
+
+        Label descriptionLabel = new Label("Não há ordens de serviço disponíveis");
+        descriptionLabel.setFont(Font.font(Font.getDefault().getFamily(), FontWeight.BOLD, 16));
+        descriptionLabel.setMaxWidth(750);
+
+        vBox.getChildren().addAll(descriptionLabel);
+
+        hBox.getChildren().addAll(vBox);
         return hBox;
     }
 }
