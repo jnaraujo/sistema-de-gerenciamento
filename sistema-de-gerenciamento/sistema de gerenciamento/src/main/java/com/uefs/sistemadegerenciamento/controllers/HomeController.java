3ca82ec3314ed656bc3e79b29a4f2e57a537bc5f
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-05-10 20:11:07 -0300

    feat: add BigButtonComponent

--- sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/controllers/HomeController.java
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/controllers/HomeController.java
@@ -1,23 +1,16 @@
 package com.uefs.sistemadegerenciamento.controllers;
 
-import com.uefs.sistemadegerenciamento.HelloApplication;
 import com.uefs.sistemadegerenciamento.constants.UserType;
 import com.uefs.sistemadegerenciamento.model.user.User;
 import com.uefs.sistemadegerenciamento.utils.PageLoader;
+import com.uefs.sistemadegerenciamento.view.components.BigButtonComponent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class HomeController {
     private User user;
@@ -71,22 +64,22 @@ public class HomeController {
     private List<Button> getTechnicianButtons(){
         List<Button> buttons = new ArrayList<>();
 
-        Button button = createButton("Listar ordens de serviço");
+        Button button = BigButtonComponent.create("Listar ordens de serviço");
         button.setOnAction(event -> {
-            WorkOrderListController controller = (WorkOrderListController) PageLoader.openPage("work_order_list.fxml");
+            WorkOrderListController controller = PageLoader.openPage("work_order_list.fxml");
             controller.setUser(user);
 
         });
         buttons.add(button);
 
-        button = createButton("Ver estoque");
+        button = BigButtonComponent.create("Ver estoque");
         button.setOnAction(event -> {
 //            InventoryController controller = (InventoryController) PageLoader.openPage("inventory.fxml");
 //            controller.setUser(user);
         });
         buttons.add(button);
 
-        button = createButton("Criar ordem de serviço");
+        button = BigButtonComponent.create("Criar ordem de serviço");
         button.setOnAction(event -> {
 //            CreateOrderController controller = (CreateOrderController) PageLoader.openPage("create_order.fxml");
 //            controller.setUser(user);
@@ -98,7 +91,7 @@ public class HomeController {
     private List<Button> getReceptionistButtons(){
         List<Button> buttons = new ArrayList<>();
 
-        Button button = createButton("Criar ordem de serviço");
+        Button button = BigButtonComponent.create("Criar ordem de serviço");
         button.setOnAction(event -> {
 //            CreateOrderController controller = (CreateOrderController) PageLoader.openPage("create_order.fxml");
 //            controller.setUser(user);
@@ -111,46 +104,19 @@ public class HomeController {
     private List<Button> getAdministratorButtons(){
         List<Button> buttons = new ArrayList<>();
 
-        Button button = createButton("Gerar relatório");
+        Button button = BigButtonComponent.create("Gerar relatório");
         button.setOnAction(event -> {
 //            CreateOrderController controller = (CreateOrderController) PageLoader.openPage("create_order.fxml");
 //            controller.setUser(user);
         });
         buttons.add(button);
 
-        button = createButton("Gerenciar usuários");
+        button = BigButtonComponent.create("Gerenciar usuários");
         button.setOnAction(event -> {
 //            ManageUsersController controller = (ManageUsersController) PageLoader.openPage("manage_users.fxml");
 //            controller.setUser(user);
         });
 
         return buttons;
-    }
-
-    private Button createButton(String text) {
-        Font font = new Font("System", 14);
-        Button button = new Button(text);
-
-        button.setFont(font);
-
-        button.setAlignment(javafx.geometry.Pos.CENTER);
-        button.setContentDisplay(javafx.scene.control.ContentDisplay.CENTER);
-        button.setMnemonicParsing(false);
-
-        button.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
-        button.setTextFill(javafx.scene.paint.Color.WHITE);
-        button.setTextOverrun(javafx.scene.control.OverrunStyle.CLIP);
-        button.wrapTextProperty().setValue(true);
-
-        button.setPrefWidth(250);
-        button.setPrefHeight(75);
-        button.setStyle("-fx-background-color: #5c677d; -fx-background-radius: 8px;");
-
-        return button;
-    }
-
-    private void openPage(String url) {
-
-
     }
 }
---
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/view/components/BigButtonComponent.java
@@ -0,0 +1,28 @@
+package com.uefs.sistemadegerenciamento.view.components;
+
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+
+public class BigButtonComponent {
+    public static Button create(String text){
+        Font font = new Font("System", 14);
+        Button button = new Button(text);
+
+        button.setFont(font);
+
+        button.setAlignment(javafx.geometry.Pos.CENTER);
+        button.setContentDisplay(javafx.scene.control.ContentDisplay.CENTER);
+        button.setMnemonicParsing(false);
+
+        button.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
+        button.setTextFill(javafx.scene.paint.Color.WHITE);
+        button.setTextOverrun(javafx.scene.control.OverrunStyle.CLIP);
+        button.wrapTextProperty().setValue(true);
+
+        button.setPrefWidth(250);
+        button.setPrefHeight(75);
+        button.setStyle("-fx-background-color: #5c677d; -fx-background-radius: 8px;");
+
+        return button;
+    }
+}
