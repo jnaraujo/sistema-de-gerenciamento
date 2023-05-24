71e477417ef7f048258ef05930a32324740ff0ce
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-05-24 19:46:20 -0300

    feat: improved home buttons

--- sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/controllers/HomeController.java
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/controllers/HomeController.java
@@ -9,8 +9,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class HomeController extends Controller {
@@ -23,7 +25,34 @@ public class HomeController extends Controller {
     private Text userRoleText;
 
     @FXML
-    private FlowPane flowPane;
+    private FlowPane workOrderFlowPane;
+
+    @FXML
+    private FlowPane customerFlowPane;
+
+    @FXML
+    private FlowPane userFlowPane;
+
+    @FXML
+    private FlowPane serviceFlowPane;
+
+    @FXML
+    private FlowPane otherFlowPane;
+
+    @FXML
+    private VBox workOrderVBox;
+
+    @FXML
+    private VBox customerVBox;
+
+    @FXML
+    private VBox userVBox;
+
+    @FXML
+    private VBox serviceVBox;
+
+    @FXML
+    private VBox mainVBox;
 
     public void setLoggedUser(User loggedUser) {
         this.loggedUser = loggedUser;
@@ -34,23 +63,66 @@ public class HomeController extends Controller {
     }
 
     private void setUpListOfUserViewsButtons() {
-        List<Button> buttons = new ArrayList<>();
+        HashMap<String, List<Button>> buttons = new HashMap<>();
+        buttons.put("workOrderButtons", new ArrayList<>());
+        buttons.put("customerButtons", new ArrayList<>());
+        buttons.put("userButtons", new ArrayList<>());
+        buttons.put("serviceButtons", new ArrayList<>());
 
         if(loggedUser.getUserType() == UserType.RECEPTIONIST) {
-            buttons.addAll(getReceptionistButtons());
+            HashMap<String, List<Button>> receptionistButtons = getReceptionistButtons();
+            for(String key : receptionistButtons.keySet()) {
+                buttons.get(key).addAll(receptionistButtons.get(key));
+            }
         } else if(loggedUser.getUserType() == UserType.TECHNICIAN) {
-            buttons.addAll(getTechnicianButtons());
+            HashMap<String, List<Button>> technicianButtons = getTechnicianButtons();
+            for(String key : technicianButtons.keySet()) {
+                buttons.get(key).addAll(technicianButtons.get(key));
+            }
         } else if(loggedUser.getUserType() == UserType.ADMINISTRATOR) {
-            buttons.addAll(getAdministratorButtons());
-            buttons.addAll(getTechnicianButtons());
-            buttons.addAll(getReceptionistButtons());
+            HashMap<String, List<Button>> administratorButtons = getAdministratorButtons();
+            for(String key : administratorButtons.keySet()) {
+                buttons.get(key).addAll(administratorButtons.get(key));
+            }
+            HashMap<String, List<Button>> technicianButtons = getTechnicianButtons();
+            for(String key : technicianButtons.keySet()) {
+                buttons.get(key).addAll(technicianButtons.get(key));
+            }
+            HashMap<String, List<Button>> receptionistButtons = getReceptionistButtons();
+            for(String key : receptionistButtons.keySet()) {
+                buttons.get(key).addAll(receptionistButtons.get(key));
+            }
         }
 
-        flowPane.getChildren().addAll(buttons);
+        if(buttons.get("workOrderButtons").size() > 0) {
+            workOrderFlowPane.getChildren().addAll(buttons.get("workOrderButtons"));
+        } else {
+            mainVBox.getChildren().remove(workOrderVBox);
+        }
+
+        if(buttons.get("customerButtons").size() > 0) {
+            customerFlowPane.getChildren().addAll(buttons.get("customerButtons"));
+        } else {
+            mainVBox.getChildren().remove(customerVBox);
+        }
+
+        if(buttons.get("userButtons").size() > 0) {
+            userFlowPane.getChildren().addAll(buttons.get("userButtons"));
+        } else {
+            mainVBox.getChildren().remove(userVBox);
+        }
+
+        if(buttons.get("serviceButtons").size() > 0) {
+            serviceFlowPane.getChildren().addAll(buttons.get("serviceButtons"));
+        } else {
+            mainVBox.getChildren().remove(serviceVBox);
+        }
     }
 
-    private List<Button> getTechnicianButtons(){
-        List<Button> buttons = new ArrayList<>();
+    private HashMap<String, List<Button>> getTechnicianButtons(){
+        HashMap<String, List<Button>> buttons = new HashMap<>();
+        buttons.put("workOrderButtons", new ArrayList<>());
+        buttons.put("serviceButtons", new ArrayList<>());
 
         Button button = BigButtonComponent.create("\uD83D\uDCCB Listar ordens de serviço");
         button.setOnAction(event -> {
@@ -58,104 +130,108 @@ public class HomeController extends Controller {
             controller.setLoggedUser(loggedUser);
 
         });
-        buttons.add(button);
+        buttons.get("workOrderButtons").add(button);
 
         button = BigButtonComponent.create("\uD83D\uDCE6 Gerenciar estoque");
         button.setOnAction(event -> {
             ManageInventoryController controller = PageLoader.openPage("manage_inventory.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("workOrderButtons").add(button);
 
         button = BigButtonComponent.create("Gerenciar Serviços de Instalação");
         button.setOnAction(event -> {
             ManageInstallationServiceController controller = PageLoader.openPage("manage_installation_service.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("serviceButtons").add(button);
 
         button = BigButtonComponent.create("Gerenciar Serviços de Limpeza");
         button.setOnAction(event -> {
             ManageCleaningServiceController controller = PageLoader.openPage("manage_cleaning_service.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("serviceButtons").add(button);
 
         return buttons;
     }
 
-    private List<Button> getReceptionistButtons(){
-        List<Button> buttons = new ArrayList<>();
+    private HashMap<String, List<Button>> getReceptionistButtons(){
+        HashMap<String, List<Button>> buttons = new HashMap<>();
+        buttons.put("workOrderButtons", new ArrayList<>());
+        buttons.put("customerButtons", new ArrayList<>());
 
         Button button = BigButtonComponent.create("\uD83D\uDD27 Criar ordem de serviço");
         button.setOnAction(event -> {
             CreateWorkOrderController controller = PageLoader.openPage("create_order.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("workOrderButtons").add(button);
 
         button = BigButtonComponent.create("\uD83E\uDDD1\u200D\uD83D\uDCBC Criar novo Cliente");
         button.setOnAction(event -> {
             CreateCustomerController controller = PageLoader.openPage("create_customer.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("customerButtons").add(button);
 
         button = BigButtonComponent.create("\uD83D\uDC65 Gerenciar clientes");
         button.setOnAction(event -> {
             ManageCustomersController controller = PageLoader.openPage("manage_customers.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("customerButtons").add(button);
 
         return buttons;
     }
 
-    private List<Button> getAdministratorButtons(){
-        List<Button> buttons = new ArrayList<>();
+    private HashMap<String, List<Button>> getAdministratorButtons(){
+        HashMap<String, List<Button>> buttons = new HashMap<>();
+        buttons.put("serviceButtons", new ArrayList<>());
+        buttons.put("customerButtons", new ArrayList<>());
+        buttons.put("userButtons", new ArrayList<>());
 
         Button button = BigButtonComponent.create("\uD83D\uDCCA Gerar relatório");
         button.setOnAction(event -> {
 //            CreateOrderController controller = (CreateOrderController) PageLoader.openPage("create_order.fxml");
 //            controller.setUser(user);
         });
-        buttons.add(button);
+        buttons.get("serviceButtons").add(button);
 
         button = BigButtonComponent.create("➕ Criar novo usuário");
         button.setOnAction(event -> {
-            CreateUserController controller = (CreateUserController) PageLoader.openPage("create_user.fxml");
+            CreateUserController controller = PageLoader.openPage("create_user.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("userButtons").add(button);
 
         button = BigButtonComponent.create("\uD83D\uDC65 Gerenciar usuários");
         button.setOnAction(event -> {
             ManageUserController controller = PageLoader.openPage("manage_user.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("userButtons").add(button);
 
         button = BigButtonComponent.create("\uD83D\uDD0C Adicionar novo componente");
         button.setOnAction(event -> {
             CreateComponentController controller = PageLoader.openPage("create_component.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("serviceButtons").add(button);
 
         button = BigButtonComponent.create("Criar novo Serviço de Instalação");
         button.setOnAction(event -> {
             CreateInstallationServiceController controller = PageLoader.openPage("create_installation_service.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
+        buttons.get("serviceButtons").add(button);
 
         button = BigButtonComponent.create("Criar novo Serviço de Limpeza");
         button.setOnAction(event -> {
             CreateCleaningServiceController controller = PageLoader.openPage("create_cleaning_service.fxml");
             controller.setLoggedUser(loggedUser);
         });
-        buttons.add(button);
-
+        buttons.get("serviceButtons").add(button);
 
         return buttons;
     }
