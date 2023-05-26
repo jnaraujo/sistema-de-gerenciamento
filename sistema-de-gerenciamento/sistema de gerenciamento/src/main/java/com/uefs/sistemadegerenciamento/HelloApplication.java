06bf020799b45699acb64d3048868b912390413b
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-05-26 19:26:33 -0300

    feat: removed dead code

--- sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/HelloApplication.java
+++ sistema-de-gerenciamento/sistema de gerenciamento/src/main/java/com/uefs/sistemadegerenciamento/HelloApplication.java
@@ -1,15 +1,11 @@
 package com.uefs.sistemadegerenciamento;
 
 import com.uefs.sistemadegerenciamento.dao.DAOManager;
-import com.uefs.sistemadegerenciamento.model.Customer;
-import com.uefs.sistemadegerenciamento.model.WorkOrder;
 import com.uefs.sistemadegerenciamento.model.user.Administrator;
-import com.uefs.sistemadegerenciamento.model.user.User;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -42,40 +38,6 @@ public class HelloApplication extends Application {
                     )
             );
         }
-
-        DAOManager.getWorkOrderDao().getAll().forEach(workOrder -> {
-            workOrder.setTechnicianId(null);
-            DAOManager.getWorkOrderDao().update(workOrder);
-        });
-
-//        Customer customer = DAOManager.getCustomerDao().save( new Customer(
-//                "Joao",
-//                "rua a, 123",
-//                "9999999999",
-//                "joao@test.com"
-//        ));
-//
-//        DAOManager.getWorkOrderDao().save(
-//                new WorkOrder(
-//                        "O cliente está reclamando de um problema no sistema. Ele suspeita que seja algo relacionado as memorias ram.",
-//                        customer.getId()
-//                )
-//        );
-//
-//        customer = DAOManager.getCustomerDao().save( new Customer(
-//                "Maria",
-//                "rua b, 456",
-//                "9999999999",
-//                "maria@test.com"
-//        ));
-//
-//        DAOManager.getWorkOrderDao().save(
-//                new WorkOrder(
-//                        "O cliente disse que o erro ocorre quando ele tenta abrir o navegador. Segundo ela, o problema começou depois que ela instalou um programa de edição de imagens.",
-//                        customer.getId()
-//                )
-//        );
-
 
         launch();
     }
